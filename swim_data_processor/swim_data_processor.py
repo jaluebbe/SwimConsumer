88a3c9396e90659ed266b8de8381e532f07cd59f
Jannis Luebbe <luebbe@opensky-network.org>
2022-01-07 18:00:08 +0100

    added discrimination between FDPS and TFMS messages.

--- swim_data_processor/swim_data_processor.py
+++ swim_data_processor/swim_data_processor.py
@@ -3,29 +3,49 @@ import json
 import logging
 
 
-redis_connection = redis.Redis('redis', decode_responses=True)
+redis_connection = redis.Redis("redis", decode_responses=True)
+
+
+def process_fdps_message(flights):
+    print(f"### FDPS message with {len(flights)} items received ###")
+    for _flight in flights:
+        if isinstance(_flight, str):
+            logging.debug(f"skipping '{_flight}'.")
+            continue
+        flight = _flight["flight"]
+        callsign = flight["flightIdentification"]["aircraftIdentification"]
+        print(f"############### {callsign} ###############")
+        print(json.dumps(flight, indent=2))
+
+
+def process_tfms_message(flights):
+    print(f"### FDPS message with {len(flights)} items received ###")
+    for flight in flights:
+        callsign = flight["acid"]
+        print(f"############### {callsign} ###############")
+        print(json.dumps(flight, indent=2))
 
 
 def process_message(message):
-    flights = message['ns5:MessageCollection']['message']
-    for _flight in flights:
-        if isinstance(_flight, str):
-            continue
-        flight = _flight['flight']
-        callsign = flight['flightIdentification']['aircraftIdentification']
-        print(f"############### {callsign} ###############")
-        print(json.dumps(flight, indent=2))
+    if message.get("ns5:MessageCollection") is not None:
+        process_fdps_message(message["ns5:MessageCollection"]["message"])
+    elif message.get("ds:tfmDataService") is not None:
+        process_tfms_message(
+            message["ds:tfmDataService"]["fltdOutput"]["fdm:fltdMessage"]
+        )
+    else:
+        print(json.dumps(message, indent=2))
 
 
 while True:
     try:
         _pubsub = redis_connection.pubsub(ignore_subscribe_messages=True)
-        _pubsub.subscribe('SWIM')
+        _pubsub.subscribe("SWIM")
         for message in _pubsub.listen():
-            if not message['type'] == 'message':
+            if not message["type"] == "message":
                 continue
-            process_message(json.loads(message['data']))
+            process_message(json.loads(message["data"]))
     except IOError:
-        logging.warning('reconnecting to Redis')
+        logging.warning("reconnecting to Redis")
     except Exception:
-        logging.exception('error occured')
+        logging.exception("error occured")
