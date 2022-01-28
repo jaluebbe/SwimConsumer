810dc9063d1ecc3001bf50068c598851f4c98e16
Jannis Luebbe <luebbe@opensky-network.org>
2022-01-28 22:29:25 +0100

    reinserted logging. Forcing termination on exception to allow a fresh instance of the program to be started.

--- swim_consumer/RedisOutput.java
+++ swim_consumer/RedisOutput.java
@@ -3,12 +3,15 @@ package com.harris.cinnato.outputs;
 import com.typesafe.config.Config;
 import nl.melp.redis.Redis;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Outputs the messages to a Redis channel
  *
  */
 public class RedisOutput extends Output {
+    private static final Logger logger = LoggerFactory.getLogger(RedisOutput.class);
     private static Redis redis_connection;
 
     public RedisOutput(Config config) throws java.net.UnknownHostException, java.io.IOException {
@@ -26,6 +29,9 @@ public class RedisOutput extends Output {
         try {
             redis_connection.call("PUBLISH", "SWIM", this.convert(message));
         }
-        catch(java.io.IOException e) {};
+        catch(java.io.IOException e) {
+            logger.error("Redis connection failed", e);
+            System.exit(-1);
+        };
     }
 }
