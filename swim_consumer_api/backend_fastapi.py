418d30a8df3d91fb69ace3925deb4e5836201be4
Jannis Lübbe <luebbe@opensky-network.org>
2024-11-28 20:51:20 +0100

    Added endpoint to request flights by callsign. Updated gitignore.

--- swim_consumer_api/backend_fastapi.py
+++ swim_consumer_api/backend_fastapi.py
@@ -1,3 +1,4 @@
+import re
 from fastapi import FastAPI, Query, HTTPException, Depends
 import arrow
 import sqlite3
@@ -5,6 +6,9 @@ import sqlite3
 app = FastAPI(openapi_prefix="", title="SwimConsumer API", description="")
 
 SWIM_DB_FILE = "swim_flight_history.sqb"
+callsign_validator = re.compile(
+    "^([A-Z]{3})[0-9](([0-9]{0,3})|([0-9]{0,2})([A-Z])|([0-9]?)([A-Z]{2}))$"
+)
 
 
 def parse_datetime(value: str):
@@ -75,6 +79,46 @@ def get_swim_flight_history(
         db_connection.row_factory = sqlite3.Row
         _cursor = db_connection.cursor()
         _cursor.execute(sql_query)
+        result = _cursor.fetchall()
+
+    result_list = []
+    for _row in result:
+        _row_dict = dict(_row)
+        _row_dict["DepartureActual"] = bool(_row_dict["DepartureActual"])
+        _row_dict["ArrivalActual"] = bool(_row_dict["ArrivalActual"])
+        result_list.append(_row_dict)
+    return result_list
+
+
+@app.get("/api/swim_flight_history_by_callsign")
+def get_swim_flight_history_by_callsign(
+    callsign: str = Query(
+        ..., description="Callsign representing an airline flight"
+    ),
+    days_back: int = Query(
+        14,
+        description=(
+            "Number of days to go back from the current date "
+            "(defaults to 14 days)"
+        ),
+    ),
+):
+    if not callsign_validator.match(callsign):
+        raise HTTPException(
+            status_code=400, detail="Unsupported callsign format"
+        )
+    begin_epoch = int(arrow.utcnow().shift(days=-days_back).timestamp())
+
+    sql_query = f"""
+        SELECT Callsign, OriginIcao AS Origin, IGTD, GateDeparture,
+        Departure, DepartureActual, DestinationIcao AS Destination,
+        Arrival, ArrivalActual, GateArrival
+        FROM SWIMFlightHistory
+        WHERE Callsign = ? AND Departure > ?;"""
+    with sqlite3.connect(SWIM_DB_FILE) as db_connection:
+        db_connection.row_factory = sqlite3.Row
+        _cursor = db_connection.cursor()
+        _cursor.execute(sql_query, (callsign, begin_epoch))
         result = _cursor.fetchall()
 
     result_list = []
